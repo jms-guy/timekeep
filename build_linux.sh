7a3bedcac00cba40810d6ec729840e2f97766d59
jms-guy <guyjamie92@gmail.com>
2025-09-26 17:10:39 -0400

    removed build files

--- build_linux.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# build-linux.sh
-GOOS=linux go build -o /tmp/Timekeep/timekeepd ./cmd/service
-echo "Binary build to /tmp/Timekeep"
-
-GOOS=linux go build -o /tmp/Timekeep/timekeep ./cmd/cli
--- build_windows.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# build-windows.sh
-GOOS=windows go build -o /mnt/c/temp/timekeep_service.exe ./cmd/service
-echo "Binary built to C:\temp\timekeep_service.exe"
-
-GOOS=windows go build -o /mnt/c/temp/timekeep.exe ./cmd/cli
